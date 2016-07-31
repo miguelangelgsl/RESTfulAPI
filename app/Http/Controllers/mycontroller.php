5fb53f446d756b3ecd372a28b43e72e5c7a8fc42
miguelangelgsl <miguelangelgsl@gmail.com>
2016-07-31 17:31:53 -0600

    Primer Commit
    Se envia la estructura completa de Laravel 5

---
+++ app/Http/Controllers/mycontroller.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+
+use App\Prueba;
+
+class MyController extends Controller{
+
+	public function index(){
+
+		$modelo=new Prueba();
+
+		$saludo= $modelo->saludar("Miguel");
+
+		return view('prueba.index',['saludo'=>$saludo]);
+	}
+
+}
