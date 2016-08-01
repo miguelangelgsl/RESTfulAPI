20f582e895c112f0f5abb75dc32d73dce3078c0d
Miguel Gutierrez <miguelangelgsl@gmail.com>
2016-07-31 23:51:22 -0600

    Se personalizan rutas y funciones
    Fabricante Controller
    Vehiculo Controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-//Route::get('/','VehiculoController@showAll');
-Route::get('/','MyController@index');
+Route::get('/','VehiculoController@showAll');
+//Route::get('/','MyController@index');
 Route::resource('fabricantes', 'FabricanteController');
 Route::resource('fabricantes.vehiculos', 'VehiculoController');
