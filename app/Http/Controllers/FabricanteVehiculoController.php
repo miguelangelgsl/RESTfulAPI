7a7f58dd04359cfa11d7cab77eaa304ba198a634
miguelangelgsl <miguelangelgsl@gmail.com>
2016-08-03 21:03:16 -0600

    Modificaciòn MiddleWare
    Se personaliza middleware

--- app/Http/Controllers/FabricanteVehiculoController.php
+++ app/Http/Controllers/FabricanteVehiculoController.php
@@ -12,7 +12,7 @@ class FabricanteVehiculoController extends Controller {
 
 
 	public function __construct(){
-		$this->middleware('auth.basic',['only' => ['store','update','destroy']]);
+		$this->middleware('auth.basic.once',['only' => ['store','update','destroy']]);
 	}
 	
 	/**
